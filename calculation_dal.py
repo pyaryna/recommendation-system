1d7a6b9c1fcab64cb43ffdea94a491d4d9459883
Yaryna <p.yaryna@gmail.com>
2022-06-02 13:38:55 +0200

    add working with rates collection

--- calculation_dal.py
+++ calculation_dal.py
@@ -1,5 +1,23 @@
+import datetime
+
 database = None
 
 def connect_to_database(db):
     global database
     database = db
+
+def get_collection(dataset):
+    cursor = database[dataset].find({})
+    return list(cursor)
+
+def get_rates_for_previous_day():    
+    yesterday_datetime = datetime.datetime.utcnow() - datetime.timedelta(days = 1)
+    yesterday = datetime.datetime(yesterday_datetime.year, yesterday_datetime.month, yesterday_datetime.day)
+    cursor = database.rates.find({'reviews.createdAt':{ '$eq': yesterday}})
+    return list(cursor)
+
+def get_rates_by_users(users):
+    cursor = database.rates.find({
+        'reviews.userId' : { '$in' : users}
+    })
+    return list(cursor)    
